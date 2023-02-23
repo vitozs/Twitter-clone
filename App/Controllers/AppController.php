e70ce6c26c6944ced8180bf2ac74cfda3531b69a
vitozs <94878000+vitozs@users.noreply.github.com>
2023-02-23 18:30:02 -0300

    autenticação de usuários

---
+++ App/Controllers/AppController.php
@@ -0,0 +1,31 @@
+<?php
+    namespace App\Controllers;
+    //os recursos do miniframework
+    use MF\Controller\Action;
+    use MF\Model\Container;
+
+    class AppController extends Action{
+
+        public function timeline(){
+
+            session_start();
+
+
+            if(!empty($_SESSION['id']) && !empty($_SESSION['nome'])){
+                $this->render('timeline');
+
+            }else{
+                header('Location: /?login=erro');
+
+
+            }
+        
+        
+        
+        
+        
+        }
+
+    }
+
+?>
