5434b10b78adca5f50d3235df0c8cb6dc7531e05
vitozs <94878000+vitozs@users.noreply.github.com>
2023-02-27 17:15:40 -0300

    feature: paginação de registros

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -13,8 +13,25 @@
             $tweet = Container::getModel('Tweet');
             $tweet->__set('id_usuario', $_SESSION['id']);
                 
-            $tweets = $tweet->getAll();
+           
+
+            //variaveis de páginação
+            $total_registros_pagina = 10;
+            $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+            $deslocamento = ($pagina - 1) * $total_registros_pagina;
+
+            $tweets = $tweet->getPorPagina($total_registros_pagina, $deslocamento);
+            
+            $total_tweets = $tweet->getTotalRegistros();
+           
+            $total_de_paginas = ceil($total_tweets['total'] / $total_registros_pagina);
+
+            $this->view->total_de_paginas = $total_de_paginas;
+            $this->view->pagina_ativa = $pagina;
+
+
             $this->view->tweets = $tweets;
+
 
             $usuario = Container::getModel('Usuario');
             $usuario->__set('id', $_SESSION['id']);
